074cdd78ec3dcefca2b108783182095dce1bb4d0
Schmidt Robert <robert.schmidt@nevexo.ro>
2023-10-23 21:09:43 +0300

    Added filtering for lower and upper boundary. Fixed variable names and "magic numbers".

--- Homeworks/Homework2/RGB_LED_potentiometer/RGB_LED_potentiometer.ino
+++ Homeworks/Homework2/RGB_LED_potentiometer/RGB_LED_potentiometer.ino
@@ -4,19 +4,29 @@ const int ledPinG = 10;
 const int ledPinB = 11;
 
 // Pins for the 3 potentiometer inputs (analog)
-const int potPinR = A5;
-const int potPinG = A4;
-const int potPinB = A3;
+const int inputPinR = A5;
+const int inputPinG = A4;
+const int inputPinB = A3;
 
 // Potentiometer values
-int potValR = 0;
-int potValG = 0;
-int potValB = 0;
+int inputValueR = 0;
+int inputValueG = 0;
+int inputValueB = 0;
 
-// 
-int oldLedValR = 0;
-int oldLedValG = 0;
-int oldLedValB = 0;
+// Old LED values to be able to compare and filter flickering
+int oldLedValueR = 0;
+int oldLedValueG = 0;
+int oldLedValueB = 0;
+
+// Constants
+const int serialBaud = 9600;
+const int mapInputStart = 0;
+const int mapInputEnd = 1023;
+const int mapOutputStart = 0;
+const int mapOutputEnd = 255;
+const int flickerInterval = 1;
+const int filterBottom = 20;
+const int filterTop = 240;
 
 // Initiate the pins and the Serial connection
 void setup() {
@@ -25,42 +35,64 @@ void setup() {
   pinMode(ledPinG, OUTPUT);
   pinMode(ledPinB, OUTPUT);
 
-  pinMode(potPinR, INPUT);
-  pinMode(potPinG, INPUT);
-  pinMode(potPinB, INPUT);
+  pinMode(inputPinR, INPUT);
+  pinMode(inputPinG, INPUT);
+  pinMode(inputPinB, INPUT);
 
-  Serial.begin(9600);
+  Serial.begin(serialBaud);
 }
 
 /* 
   Take the analog inputs from the potentiometers.
   Convert it to analog outputs (from 0-1023 to 0-255).
   Filter the inputs to take into account the fluctuation between two adjacent values.
+  Filter the upper and lower boundary to avoid flickering.
   Write the values into the RGB channels using PWM digital output to emulate an analog output.
 */
 void loop() {
 
-  potValR = analogRead(potPinR);
-  potValG = analogRead(potPinG);
-  potValB = analogRead(potPinB);
+  inputValueR = analogRead(inputPinR);
+  inputValueG = analogRead(inputPinG);
+  inputValueB = analogRead(inputPinB);
 
   // Input mapping from 0-1023 to 0-255
-  int ledValR = map(potValR, 0, 1023, 0, 255);
-  int ledValG = map(potValG, 0, 1023, 0, 255);
-  int ledValB = map(potValB, 0, 1023, 0, 255);
+  int ledValueR = map(inputValueR, mapInputStart, mapInputEnd, mapOutputStart, mapOutputEnd);
+  int ledValueG = map(inputValueG, mapInputStart, mapInputEnd, mapOutputStart, mapOutputEnd);
+  int ledValueB = map(inputValueB, mapInputStart, mapInputEnd, mapOutputStart, mapOutputEnd);
 
-  // input filtering
-  if (abs(ledValR - oldLedValR) > 1 || abs(ledValG - oldLedValG) > 1 || abs(ledValB - oldLedValB) > 1) {
+  // Input filtering for fluctuations
+  if ((abs(ledValueR - oldLedValueR) > flickerInterval || abs(ledValueG - oldLedValueG) > flickerInterval || abs(ledValueB - oldLedValueB) > flickerInterval)) {
 
-    oldLedValR = ledValR;
-    oldLedValG = ledValG;
-    oldLedValB = ledValB;
+    oldLedValueR = ledValueR;
+    oldLedValueG = ledValueG;
+    oldLedValueB = ledValueB;
 
-    analogWrite(ledPinR, ledValR);
-    analogWrite(ledPinG, ledValG);
-    analogWrite(ledPinB, ledValB);
+    // Output filtering for flickering
+    if (ledValueR < filterBottom ) {
+      analogWrite(ledPinR, 0);
+    } else if (ledValueR > filterTop) {
+      analogWrite(ledPinR, 255);
+    } else {
+      analogWrite(ledPinR, ledValueR);
+    }
 
+    if (ledValueG < filterBottom) {
+      analogWrite(ledPinG, 0);
+    } else if (ledValueG > filterTop) {
+      analogWrite(ledPinG, 255);
+    } else {
+      analogWrite(ledPinG, ledValueG);
+    }
+
+    if (ledValueB < filterBottom) {
+      analogWrite(ledPinB, 0);
+    } else if (ledValueB > filterTop) {
+      analogWrite(ledPinB, 255);
+    } else{
+      analogWrite(ledPinB, ledValueB);
+    }
+    
     // Serial print to show the actual values for creating custom colors
-    Serial.println((String) "R:" + ledValR + "   G:" + ledValG + "   B:" + ledValB);
+    Serial.println((String) "R:" + ledValueR + "   G:" + ledValueG + "   B:" + ledValueB);
   }
 }
